e6154d384a1e01bbd0d0ce0f42b5442dd37bd12f
kiran1926 <kiranroge19@gmail.com>
2025-03-06 13:19:32 -0600

    mongoose-lab submitted

---
+++ app.js
@@ -0,0 +1,76 @@
+const dotenv = require("dotenv"); //env import
+dotenv.config();
+
+const mongoose = require("mongoose"); //mongoose import
+
+const prompt = require("prompt-sync")(); //user input import
+
+const Customer = require("./models/customer"); // customer model import
+
+const {
+  createCustomer,
+  viewCustomers,
+  updateCustomer,
+  deleteCustomer,
+} = require("./queries");
+
+//connect MongoDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("connected to MongoDB");
+
+    const username = prompt("\nWhat is your name?");
+    console.log("Hi", username);
+
+    let exit = false;
+
+    while (!exit) {
+      const selection = prompt(`
+            Welcome to the CRM
+            
+            What would you like to do ? 
+
+            1. Create a customer
+            2. View all customers
+            3. Update a customer
+            4. Delete a customer
+            5. Quit
+            
+            Select the number of the action: 
+            `);
+      switch (selection) {
+        case "1":
+          await createCustomer();
+          break;
+        case "2":
+          await viewCustomers();
+          break;
+        case "3":
+          await updateCustomer();
+          break;
+        case "4":
+          await deleteCustomer();
+          break;
+        case "5":
+          exit = true;
+          console.log("\n Successfully logged out");
+          break;
+        default:
+          console.log(
+            "Wrong selection!! Please select from the above options numbers"
+          );
+      }
+    }
+
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB");
+
+    process.exit();
+  } catch (error) {
+    console.error("MongoDb connection error: ", error);
+    process.exit(1);
+  }
+};
+
+connect();
---
+++ queries.js
@@ -0,0 +1,90 @@
+const Customer = require("./models/customer");
+const prompt = require("prompt-sync")();
+
+//create a new customer
+
+const createCustomer = async () => {
+  console.log("\nCreating a customer....");
+  const customerData = {
+    name: prompt("Enter customer name: "),
+    age: parseInt(prompt("Enter customer age: "), 10),
+  };
+  try {
+    const customer = await Customer.create(customerData);
+    console.log("New customer created", customer);
+  } catch (error) {
+    console.log("\nError creating customer", error.message);
+  }
+};
+
+//Read: view all cusotmers
+const viewCustomers = async () => {
+  try {
+    const customers = await Customer.find({});
+    console.log("Below is a list of customers: ");
+    customers.forEach((customer) => {
+      console.log(
+        `id: ${customer.id} -- Name: ${customer.name}, Age: ${customer.age}`
+      );
+    });
+  } catch (error) {
+    console.error("\nError getting customers: ", error.message);
+  }
+};
+
+//update customer
+const updateCustomer = async () => {
+  await viewCustomers();
+
+  const customerId = prompt(
+    "\nCopy and paste the id of the customer you would like to update here: "
+  );
+  try {
+    const customer = await Customer.findById(customerId);
+
+    if (!customer) {
+      console.log("\nCustomer not found");
+      return;
+    }
+
+    const updateName = prompt("What is the customer's new name?");
+    const updateAge = parseInt(prompt("What is the customer's new age? "), 10);
+
+    customer.name = updateName;
+    customer.age = updateAge;
+    await customer.save();
+
+    console.log("\nCustomer updated successfully!");
+  } catch (error) {
+    console.error("\nError updating customer: ", error.message);
+  }
+};
+
+//delete customer
+
+const deleteCustomer = async () => {
+  await viewCustomers();
+
+  const customerId = prompt(
+    "\nCopy and paste the id of the customer you would like to delete here: "
+  );
+  try {
+    const deletedCustomer = await Customer.findByIdAndDelete(customerId);
+
+    if (!deletedCustomer) {
+      console.log("Customer not found");
+      return;
+    }
+    console.log("\nCustomer deleted successfully!");
+  } catch (error) {
+    console.error("\nError deleting customer", error.message);
+  }
+};
+
+//export
+module.exports = {
+  createCustomer,
+  viewCustomers,
+  updateCustomer,
+  deleteCustomer,
+};
